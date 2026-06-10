1c3ba4ba85a1d1d7be13048bd16a18cf5d19c0c9
Isaac Schwarz <isaac@steveandkureen.com>
2026-06-09 18:57:21 -0700

    qol

--- scripts/sessionizer.sh
+++ scripts/sessionizer.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/bash
 
-selected=$(find ~/Dev ~/ ~/.config ~/Dev/os -mindepth 1 -maxdepth 1 -type d | \
-    sed "s|^$HOME/||" | \
-    fzf
-)
-
-selected="$HOME/$selected"
+if [[ $# -eq 1 ]]; then
+    selected="$1"
+else
+    selected=$(find ~/Dev ~/ ~/.config ~/Dev/os -mindepth 1 -maxdepth 1 -type d | \
+        sed "s|^$HOME/||" | \
+        sort -u | \
+        fzf
+    )
+    [[ -n $selected ]] && selected="$HOME/$selected"
+fi
 
 if [[ -z $selected ]]; then
     exit 0
 fi
 
-selected_name=$(basename "$selected" | tr . _)
-tmux_running=$(pgrep tmux)
+selected_name=$(basename "$selected" | tr '. ' '__')
 
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c $selected
-    exit 0
+if ! tmux has-session -t="$selected_name" 2> /dev/null; then
+    tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds $selected_name -c $selected 
-    # select first window
-    tmux select-window -t $selected_name:0
+if [[ -z $TMUX ]]; then
+    tmux attach-session -t "$selected_name"
+else
+    tmux switch-client -t "$selected_name"
 fi
-
-tmux switch-client -t $selected_name
